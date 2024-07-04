1db138e21769f6c1900626bfa2cd984c6c33e03a
ddangme <ssilver121@naver.com>
2024-07-04 11:17:32 +0900

    init project

---
+++ src/main/java/com/kakao_todo/KakaoTodoApplication.java
@@ -0,0 +1,13 @@
+package com.kakao_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoTodoApplication.class, args);
+	}
+
+}
